e2c8ae564ed333ffd092c7b8e956549db527d5b9
Mandy Pilaud <mandyypilaud@gmail.com>
2018-09-27 18:49:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector('#nested.target')
+}
+
+function increaseRankBy(n)
+  
+
+function deepestChild(){
+ return document.querySelectorAll()
+}
+
+//<main id="app">
+  //<ul class="ranked-list">
+    //<li>1</li>
+    //<li>2</li>
+  //</ul>
+ 
+ // <ul class="ranked-list">
+    //<li>10</li>
+    //<li>11</li>
+  //</ul>
+//</main>//
