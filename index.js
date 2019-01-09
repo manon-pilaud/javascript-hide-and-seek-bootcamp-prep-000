74b200d6683347082f6a7e5cbcbdac63977b039c
Manon Pilaud <mandyypilaud@gmail.com>
2019-01-08 19:42:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+
+function increaseRankBy(n) {
+  const rankedList = document.querySelectorAll('.ranked-list')
+  for (let i = 0; i < rankedList.length; i++){
+    rankedList[i].innerHTML = parseInt((i + n))
+  }
+}
+
+function deepestChild() {
+  let firstNode = document.getElementById('grand-node');
+  let nextNode = firstNode.children[0];
+
+  while (nextNode) {
+    firstNode = nextNode;
+    nextNode = firstNode.children[0];
+  }
+
+  return firstNode;
+}
